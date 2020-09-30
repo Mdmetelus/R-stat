275c04d848aab5d36ad3ef23d9626a06def30a88
Mdmetelus <Mdmetelus@gmail.com>
2020-09-30 15:37:34 -0400

    second commit

--- R_for_Beginers/first_lines.R
+++ R_for_Beginers/first_lines.R
@@ -11,3 +11,19 @@ z
 print(z)
 rm(z)
 rm(list = ls()) # "remove the variables/ clear the complete workspace"
+v<-(10+17)
+yy<-77
+print(yy)
+
+getwd()
+
+setwd("")
+
+100:127
+1:6
+die<- 1:6
+dice<-die
+
+dice*dice
+
+doubleDice<-dice*dice
